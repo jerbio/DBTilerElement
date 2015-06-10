6c2d548856146dd06cf933d21dbb5700890b8098
Jerome Biotidara <jeromebiotidara@gmail.com>
2015-06-10 02:00:45 +0000

    Master Branch Initialization

---
+++ DB_Procrastination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TilerElements;
+
+namespace DBTilerElement
+{
+    public class DB_Procrastination : Procrastination
+    {
+        public DB_Procrastination(DateTimeOffset FromTimeData, DateTimeOffset BeginTimeData, int DisLikedSection)
+        {
+            FromTime = FromTimeData;
+            BeginTIme = BeginTimeData;
+            SectionOfDay = DisLikedSection;
+        }
+    }
+}
---
+++ GoogleSubCalendarEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TilerElements;
+
+namespace DBTilerElement
+{
+    public class GoogleSubCalendarEvent : SubCalendarEvent
+    {
+        public GoogleSubCalendarEvent(SubCalEvent SubCalData)
+        {
+            DateTimeOffset Start = (new DateTimeOffset()).Add(TilerElementExtension.StartOfTimeTimeSpan).AddMilliseconds(SubCalData.SubCalStartDate);
+            DateTimeOffset End = (new DateTimeOffset()).Add(TilerElementExtension.StartOfTimeTimeSpan).AddMilliseconds(SubCalData.SubCalEndDate);
+            this.EventName = SubCalData.SubCalCalendarName != null ? SubCalData.SubCalCalendarName : "";
+            StartDateTime = Start;
+            EndDateTime = End;
+            BusyFrame = new BusyTimeLine(SubCalData.ID, Start, End);
+            CalendarEventRange = new TimeLine(Start, End);
+            UniqueID = new EventID(SubCalData.ID);
+            RigidSchedule = true;
+            EventPreDeadline = new TimeSpan();
+            Priority = SubCalData.Priority;
+            ConflictingEvents = new ConflictProfile();
+            Enabled = true;
+            Complete = false;
+            EventDuration = BusyFrame.TimelineSpan;
+            LocationInfo= new Location_Elements();
+            ThirdPartyFlag = true;
+            ThirdPartyTypeInfo = ThirdPartyControl.CalendarTool.Google;
+            ThirdPartyUserIDInfo = SubCalData.ThirdPartyUserID;
+            CreatorIDInfo = SubCalData.ThirdPartyUserID;
+            otherPartyID = SubCalData.ThirdPartyEventID;
+        }
+
+        static public SubCalendarEvent convertFromGoogleToSubCalendarEvent(SubCalEvent SubCalEventData)
+        {
+            SubCalendarEvent RetValue = new GoogleSubCalendarEvent(SubCalEventData);
+            return RetValue;
+        }
+    }
+
+}
