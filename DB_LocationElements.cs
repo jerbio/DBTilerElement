f8193431cf40b6f20b74a0cf7623eb753e4771e2
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-03-21 16:43:51 -0400

    Made the classes more stable. It compiles but we need to add more classes to the DB. Also, you might need to remove some of the constructors to avoid code confusion down the road. You cannot update the database until you are done adding all the tables.

--- DB_LocationElements.cs
+++ DB_LocationElements.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using TilerElements;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TilerFront;
+//using TilerFront;
 namespace DBTilerElement
 {
     [Table("Locations")]
