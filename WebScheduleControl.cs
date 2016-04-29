fa29a6f8e9189c4c8ee45f27e485d30c2a0804dd
Jerome Biotidara <jeromebiotidara@gmail.com>
2016-04-29 02:19:06 -0400

    Created new context for database absstraction. Also added new schedulecontrol child class

---
+++ WebScheduleControl.cs
@@ -0,0 +1,32 @@
+﻿#define UseDefaultLocation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using TilerElements.Wpf;
+using TilerElements.DB;
+using System.Threading.Tasks;
+//using DBTilerElement;
+using System.Diagnostics;
+#if ForceReadFromXml
+#else
+//using CassandraUserLog;
+using TilerSearch;
+#endif
+
+
+
+namespace DBTilerElement
+{
+    public class WebScheduleControl:ScheduleControl
+    {
+        public WebScheduleControl()
+        {
+            DataBase = new ScheduleContext();
+        }
+    }
+
+}
