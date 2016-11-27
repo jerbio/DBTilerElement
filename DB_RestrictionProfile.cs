d1c50dcfa3b4627694f540047e3c90d906a86b97
Jerome Biotidara <Jeromebiotidara@gmail.com>
2016-11-26 22:10:39 -0500

    Updated varaible names to use restriction day as opposed to tuple of restriction timeline and dayofweek

--- DB_RestrictionProfile.cs
+++ DB_RestrictionProfile.cs
@@ -8,15 +8,15 @@ namespace DBTilerElement
 {
     public class DB_RestrictionProfile : RestrictionProfile
     {
-        public DB_RestrictionProfile(List<Tuple<DayOfWeek, RestrictionTimeLine>> RestrictionTimeLineData)
+        public DB_RestrictionProfile(List<RestrictionDay> RestrictionTimeLineData)
         {
-            this.DaySelection = new Tuple<DayOfWeek, RestrictionTimeLine>[7];
-            foreach (Tuple<DayOfWeek, RestrictionTimeLine> eachTuple in RestrictionTimeLineData)
+            this.DaySelection = new RestrictionDay[7];
+            foreach (RestrictionDay eachTuple in RestrictionTimeLineData)
             {
-                DaySelection[(int)eachTuple.Item1] = eachTuple;
+                DaySelection[(int)eachTuple.DayOfWeek] = eachTuple;
             }
 
-            this.NoNull_DaySelections = RestrictionTimeLineData.OrderBy(obj => obj.Item1).ToArray();
+            this.NoNull_DaySelections = RestrictionTimeLineData.OrderBy(obj => obj.DayOfWeek).ToArray();
             InitializeOverLappingDictionary();
         }
     }
