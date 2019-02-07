149d728610ef91b318b9f479d267ba55e6e88799
Jerome Biotidara <jeromebiotidara@gmail.com>
2019-02-06 21:15:36 -0700

    Addressed event upadte bug, still need to implement functionality

---
+++ DB_Repetition.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TilerElements;
+
+namespace DBTilerElement
+{
+    public class DB_Repetition:Repetition
+    {
+        public override bool isPersistable { get; set; } = false;
+        public DB_Repetition (CalendarEvent parentCalendarEvent, IEnumerable<CalendarEvent> calEvents, int dayOfWeek = 7, bool notSubRepetition = false  )
+        {
+            if(calEvents.Count() > 0 && !notSubRepetition)
+            {
+                if(calEvents.First().Calendar_EventID.getRepeatDayCalendarEventComponent() != "7")
+                {
+                    initializeSubRepetitions(parentCalendarEvent, calEvents);
+                }
+            } else
+            {
+                RepetitionWeekDay = dayOfWeek;
+                _RepetitionFrequency = Frequency.NONE;
+                _RepetitionRange = new TimeLine();
+                _EnableRepeat = false;
+                _Location = new TilerElements.Location();
+                _initializingRange = new TimeLine();
+                _DictionaryOfIDAndCalendarEvents = new SubEventDictionary<string, CalendarEvent>(calEvents);
+                _DictionaryOfWeekDayToRepetition = new SubEventDictionary<int, Repetition>();
+            }
+        }
+
+        public void initializeSubRepetitions (CalendarEvent parentCalendarEvent, IEnumerable<CalendarEvent> calEvents)
+        {
+            Dictionary<DayOfWeek, List<CalendarEvent>> dayOfWeekTOCalEVents = Weekdays.ToDictionary(day => day, day => new List<CalendarEvent>());
+
+            List<Repetition> subRepetitions = dayOfWeekTOCalEVents.Where(kvp => kvp.Value.Count > 0).Select(kvp => ((Repetition)new DB_Repetition(parentCalendarEvent, kvp.Value, (int)kvp.Key, true))).ToList();
+
+            this.SubRepetitions = subRepetitions;
+        }
+    }
+}
